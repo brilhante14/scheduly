77332fdaac71152196fb690e267f33700fa850cf
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-03-18 11:07:40 -0300

    feat: Página de confirmação de agendamento

--- src/pages/Schedule/ConfirmSchedule/index.tsx
+++ src/pages/Schedule/ConfirmSchedule/index.tsx
@@ -1,5 +1,9 @@
-import dayjs from "dayjs";
-import { CalendarBlank, Clock } from "phosphor-react";
+import { TextField } from "@mui/material";
+import { TimePickerProps } from "@mui/x-date-pickers";
+import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
+import dayjs, { Dayjs } from "dayjs";
+import { CalendarBlank } from "phosphor-react";
+import { useState } from "react";
 
 import "./styles.css";
 
@@ -9,43 +13,49 @@ interface IConfirmScheduleProps {
 }
 
 export function ConfirmSchedule({ schedulingDate, returnToCalendarView }: IConfirmScheduleProps) {
-  const describredDate = dayjs(schedulingDate).format("DD[ de ]MMMM[ de ]YYYY");
-  const describedTime = dayjs(schedulingDate).format("HH:mm[h]");
+  const [startDate, setStartDate] = useState<Date | null>(schedulingDate);
+
+  const disableBeforeStart = (date: Dayjs) => {
+    return date.isBefore(startDate, "day");
+  };
+
+  const shouldDisableTime: TimePickerProps<Dayjs>['shouldDisableTime'] = (
+    value,
+    view,
+  ) => (
+    (view === "hours" && value.hour() < dayjs(startDate).get("hour")) ||
+    (view === 'minutes' && value.minute() < dayjs(startDate).get("minute"))
+  );
 
   return (
     <form className="confirmSchedule-container" onSubmit={() => { }}>
       <div className="confirmSchedule-header">
-        <p>
-          <CalendarBlank />
-          {describredDate}
-        </p>
-        <p>
-          <Clock />
-          {describedTime}
-        </p>
+        <CalendarBlank />
+        Marque seu agendamento
       </div>
 
-      <label>
-        <p>Título</p>
-        <input placeholder="Seu nome" />
-      </label>
+      <TextField label="Título" />
+      <DateTimePicker
+        label="Ínicio"
+        ampm={false}
+        value={dayjs(startDate)}
+        onChange={(value) => setStartDate(value?.toDate() || null)}
+        disablePast
+      // shouldDisableDate={afterStart}
+      // shouldDisableTime={shouldDisableTime}
+      />
 
-      <label>
-        <p>Ínicio</p>
-        <input type="datetime-local" />
-
-      </label>
-
-      <label>
-        <p>Fim</p>
-        <input type="datetime-local" />
-      </label>
+      <DateTimePicker
+        label="Fim"
+        ampm={false}
+        shouldDisableTime={shouldDisableTime}
+        shouldDisableDate={disableBeforeStart}
+      />
 
       <div className="confirmSchedule-formActions">
         <button type="button" onClick={returnToCalendarView}>Cancelar</button>
-        <button type="submit">
-          Confirmar
-        </button>
+
+        <button type="submit">Confirmar</button>
       </div>
     </form>
   );
