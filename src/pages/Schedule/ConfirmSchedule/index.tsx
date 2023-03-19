a440ff0821db3e381e3e6385584cebfb0e400b31
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-03-19 19:46:50 -0300

    feat: Função de agendamento

--- src/pages/Schedule/ConfirmSchedule/index.tsx
+++ src/pages/Schedule/ConfirmSchedule/index.tsx
@@ -4,6 +4,7 @@ import { DateTimePicker } from "@mui/x-date-pickers/DateTimePicker";
 import dayjs, { Dayjs } from "dayjs";
 import { CalendarBlank } from "phosphor-react";
 import { useState } from "react";
+import { IMeeting } from "../../Agenda";
 
 import "./styles.css";
 
@@ -13,33 +14,55 @@ interface IConfirmScheduleProps {
 }
 
 export function ConfirmSchedule({ schedulingDate, returnToCalendarView }: IConfirmScheduleProps) {
-  const [startDate, setStartDate] = useState<Date | null>(schedulingDate);
+  const [scheduleForm, setScheduleForm] = useState<IMeeting>({
+    title: "",
+    startDate: schedulingDate,
+    endDate: null,
+  });
 
   const disableBeforeStart = (date: Dayjs) => {
-    return date.isBefore(startDate, "day");
+    return date.isBefore(scheduleForm.startDate, "day");
   };
 
   const shouldDisableTime: TimePickerProps<Dayjs>['shouldDisableTime'] = (
     value,
     view,
   ) => (
-    (view === "hours" && value.hour() < dayjs(startDate).get("hour")) ||
-    (view === 'minutes' && value.minute() < dayjs(startDate).get("minute"))
+    value.isSame(scheduleForm.startDate, "day") &&
+    ((view === "hours" && value.hour() < dayjs(scheduleForm.startDate).get("hour")) ||
+      ((value.hour() === dayjs(scheduleForm.startDate).get("hour")) && value.minute() < dayjs(scheduleForm.startDate).get("minute")))
+    // (view === 'minutes' && value.minute() < dayjs(scheduleForm.startDate).get("minute"))
   );
 
+  function scheduleAMeeting() {
+    const storage = localStorage.getItem("meetings");
+    let storedMeetings = storage ? JSON.parse(storage) : [];
+
+    storedMeetings.push(scheduleForm);
+
+    localStorage.setItem("meetings", JSON.stringify(storedMeetings));
+
+    returnToCalendarView();
+  }
+
   return (
-    <form className="confirmSchedule-container" onSubmit={() => { }}>
+    <form className="confirmSchedule-container" onSubmit={scheduleAMeeting}>
       <div className="confirmSchedule-header">
         <CalendarBlank />
         Marque seu agendamento
       </div>
 
-      <TextField label="Título" />
+      <TextField
+        label="Título"
+        value={scheduleForm.title}
+        onChange={(e) => setScheduleForm(prevState => ({ ...prevState, title: e.target.value }))}
+      />
+
       <DateTimePicker
         label="Ínicio"
         ampm={false}
-        value={dayjs(startDate)}
-        onChange={(value) => setStartDate(value?.toDate() || null)}
+        value={dayjs(scheduleForm.startDate)}
+        onChange={(value) => setScheduleForm(prevState => ({ ...prevState, startDate: value?.toDate() || null }))}
         disablePast
       // shouldDisableDate={afterStart}
       // shouldDisableTime={shouldDisableTime}
@@ -48,6 +71,8 @@ export function ConfirmSchedule({ schedulingDate, returnToCalendarView }: IConfi
       <DateTimePicker
         label="Fim"
         ampm={false}
+        value={dayjs(scheduleForm.endDate)}
+        onChange={(value) => setScheduleForm(prevState => ({ ...prevState, endDate: value?.toDate() || null }))}
         shouldDisableTime={shouldDisableTime}
         shouldDisableDate={disableBeforeStart}
       />
