399e2d7e4329129134ec9df4656e796d1defe6f7
Gabriel Brilhante <joao.brilhante@alu.ufc.br>
2023-03-20 10:29:02 -0300

    feat: Função de remover Agendamento

--- src/pages/Agenda/index.tsx
+++ src/pages/Agenda/index.tsx
@@ -1,30 +1,25 @@
 import dayjs from "dayjs";
 import { PencilSimple, PlusCircle, Trash } from "phosphor-react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { sortingByDateAsc, sortingByDateDesc, sortingByTitle } from "../../utils/sorting-methods";
 
 import "./styles.css";
 
 export interface IMeeting {
+  id: string;
   title: string;
-  start: Date;
-  end: Date;
+  startDate: Date | null;
+  endDate: Date | null;
 }
 
 export function Agenda() {
-  const meetingsStored = [{
-    title: "Gym",
-    start: new Date("2023-03-18T14:00:00"),
-    end: new Date("1995-03-18T15:00:00"),
-  }, {
-    title: "School",
-    start: new Date("2023-03-18T15:00:00"),
-    end: new Date("1995-03-18T15:10:00"),
-  }];
+  // const meetingsStored = [];
+  const storage = localStorage.getItem("meetings");
+  const meetingsStored: IMeeting[] = storage ? JSON.parse(storage) : [];
 
-  const [sortingBy, setSortingBy] = useState<"title" | "dateStart" | "dateEnd">("title");
   const [meetings, setMeetings] = useState<IMeeting[]>(meetingsStored);
+  const [sortingBy, setSortingBy] = useState<"title" | "dateStart" | "dateEnd">("title");
 
   const sortMethods = {
     title: sortingByTitle,
@@ -36,9 +31,25 @@ export function Agenda() {
     setMeetings(meetingsStored.filter(meeting => meeting.title.toLowerCase().includes(e.target.value.toLowerCase())));
   }
 
+  function removeAMeeting(id: string) {
+    const storage = localStorage.getItem("meetings");
+    let storedMeetings = storage ? JSON.parse(storage) : [];
+
+    storedMeetings = storedMeetings.filter((meeting: IMeeting) => meeting.id !== id);
+
+    setMeetings(storedMeetings);
+    localStorage.setItem("meetings", JSON.stringify(storedMeetings));
+  }
+
   return (
     <div className="agenda-container">
-      <Link to="/schedule" className="agenda-scheduleButton">Novo agendamento <PlusCircle /> </Link>
+      <Link
+        to="/schedule"
+        className="agenda-scheduleButton"
+      >
+        Novo agendamento&nbsp;
+        <PlusCircle />
+      </Link>
 
       <input
         className="agenda-searchBar"
@@ -48,20 +59,35 @@ export function Agenda() {
 
       <div className="agenda-orderContainer">
         Ordenar por:
-        <button onClick={() => setSortingBy("title")}>Título</button>
-        <button onClick={() => setSortingBy("dateStart")}>Data de início</button>
-        <button onClick={() => setSortingBy("dateEnd")}>Data de fim</button>
+        <button
+          className={sortingBy === "title" ? "selected" : ""}
+          onClick={() => setSortingBy("title")}
+        >
+          Título
+        </button>
+        <button
+          className={sortingBy === "dateStart" ? "selected" : ""}
+          onClick={() => setSortingBy("dateStart")}
+        >
+          Data de início
+        </button>
+        <button
+          className={sortingBy === "dateEnd" ? "selected" : ""}
+          onClick={() => setSortingBy("dateEnd")}
+        >
+          Data de fim
+        </button>
       </div>
 
       <div className="agenda-list">
-        {meetings.sort(sortMethods[sortingBy]).map((meeting, index) => {
+        {meetings.sort(sortMethods[sortingBy]).map(meeting => {
           return (
-            <div key={index} className="agenda-item">
+            <div key={meeting.id} className="agenda-item">
               {meeting.title}
-              <span> {dayjs(meeting.start).format("DD/MM/YY[ - ]HH:mm")} | {dayjs(meeting.end).format("DD/MM/YY[ - ]HH:mm")} </span>
+              <span> {dayjs(meeting.startDate).format("DD/MM/YY[ - ]HH:mm")} | {dayjs(meeting.endDate).format("DD/MM/YY[ - ]HH:mm")} </span>
               <div className="agenda-itemButtons">
                 <button><PencilSimple /></button>
-                <button><Trash /></button>
+                <button onClick={() => removeAMeeting(meeting.id)}><Trash /></button>
               </div>
             </div>
           );
